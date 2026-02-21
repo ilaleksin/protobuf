b8fc7d41ba1124d9da51bf4b5e18085c34360f2a
Ilia Aleksin <ilya.alexin@gmail.com>
2026-02-21 22:02:02 +0100

    int64 as numbers

--- encoding/protojson/encode.go
+++ encoding/protojson/encode.go
@@ -316,14 +316,12 @@ func (e encoder) marshalSingular(val protoreflect.Value, fd protoreflect.FieldDe
 		e.WriteUint(val.Uint())
 
 	case protoreflect.Uint64Kind, protoreflect.Fixed64Kind:
-		// 64-bit integers are written out as JSON string.
 		if e.opts.UseInt64Numbers {
 			e.WriteUint(val.Uint())
 		} else {
 			e.WriteString(val.String())
 		}
 	case protoreflect.Int64Kind, protoreflect.Sint64Kind, protoreflect.Sfixed64Kind:
-		// 64-bit integers are written out as JSON string.
 		if e.opts.UseInt64Numbers {
 			e.WriteInt(val.Int())
 		} else {
